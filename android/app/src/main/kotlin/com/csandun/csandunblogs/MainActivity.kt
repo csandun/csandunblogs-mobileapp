6087ad5211e267fad7bf531b039c5004b8109412
chathuranga <csandun.personal@gmail.com>
2023-03-05 00:25:27 +0530

    First release

---
+++ android/app/src/main/kotlin/com/csandun/csandunblogs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.csandun.csandunblogs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -38,4 +38,33 @@ class News {
       });
     }
   }
+
+  Future getTags() async {
+
+    String csandunblogsApiEndpoint =
+        'https://csandunblogs.com/ghost/api/content/tags/?key=c43a16ed5e7ad7730c4f0c8b96&fields=id,name,slug,description,feature_image,accent_color,url&include=count.posts&limit=all';
+
+    http.Client client = http.Client();
+    http.Response response =
+        await client.get(Uri.parse(csandunblogsApiEndpoint));
+
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+
+      jsonData['tags'].forEach((element) {
+        if (element['name'] != "" &&
+            element['url'] != "" ) {
+          ArticleModel articleModel = ArticleModel(
+            publishedDate: element['published_at'].toString().split('T')[0],
+            publishedTime: element['published_at'].toString(),
+            image: element['feature_image'].toString(),
+            content: element['excerpt'].toString(),
+            fullArticle: element['url'].toString(),
+            title: element['title'].toString(),
+          );
+          news.add(articleModel);
+        }
+      });
+    }
+  }
 }
